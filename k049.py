9513a2eaf3166ded2c95b1375d960d32fc4d42ae
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-11-08 13:39:59 +0900

    pipが使えない時の対応方法を追記

--- k049.py
+++ k049.py
@@ -4,6 +4,7 @@
 実行時に「ModuleNotFoundError: No module named 'matplotlib'」と出力される場合は、matplotlibパッケージをインストールしてから実行結果を確認してください。
 インストール方法は環境によって異なります。
 Anacondaの場合は「conda」コマンド、Pythonをインストールして環境を整えた場合は「pip」コマンドでインストールができます。
+もし「pip」コマンドが使えない場合は、「py -m pip install パッケージ名」を試してください。
 詳細なインストール方法はインターネットを検索してください。
 
 [ データ ]
